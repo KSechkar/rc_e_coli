0b61d929bd902f0dafb53aa33a1d828beb7132f3
KSechkar <63181035+KSechkar@users.noreply.github.com>
2023-04-08 20:24:01 +0100

    Fix fig2a

--- figures/fig2a.m
+++ figures/fig2a.m
@@ -12,7 +12,7 @@
 addpath(genpath('..'))
 
 clear
-close all
+%close all
 
 %% VECTOR of fitted parameter values
 
@@ -71,12 +71,9 @@ sim.opt = odeset('reltol',1.e-6,'abstol',1.e-9); % more lenient integration tole
 
 
 %% GET model predictions with fitted parameters
-ymodel=dream_modelfun(theta,data.xdata,sim,Delta,Max_iter);
+ymodel=scaledaa_modelfun(theta,data.xdata,sim,Delta,Max_iter);
 
 disp(['SOS=',num2str(sum((ymodel-data.ydata).^2))]) % print resultant sum of squared errors
-
-% get points for original/strarting parameter values (optional)
-ymodel_origin=dream_modelfun(theta_origin,data.xdata,sim,Delta,Max_iter);
 
 %% COLOURS FOR THE PLOT 
 
@@ -199,3 +196,75 @@ axis square
 grid on
 box on
 hold off
+
+%% FUNCTION for obtaining our cell model's predictions
+% Based on dream_modelfun but uses a scaled value of a_a
+
+function ymodel=scaledaa_modelfun(theta,xdata,sim,Delta,Max_iter)
+    % reset parameters and initial condition
+    sim=sim.set_default_parameters();
+    sim=sim.set_default_init_conditions();
+
+    % change fitted parameters to current values
+    sim.parameters('a_a') = 3.881e5; % metabolic prot. transcription rate (/h)
+    sim.parameters('a_r') = sim.parameters('a_a').*theta(1); % ribosome transcription rate (/h) - rescaled!
+    sim.parameters('nu_max') = theta(2); % max metabolic rate (/h)
+    sim.parameters('K_e') = theta(3); % elongation rate Hill constant (nM)
+    sim.parameters('K_nut') = theta(3); % tRNA charging rate Hill constant (nM)
+    sim.parameters('kcm') = theta(4); % chloramphenical binding rate constant (/h/nM)
+    
+    % find steady state values for every input
+    ymodel=zeros([size(xdata,1) 2]); % initialise array of avlues predicted by the model
+    for i=1:size(xdata,1)
+        %disp(i)
+        % change parameter values to relevant inputs
+        sim.init_conditions('s')=xdata(i,1);
+        sim.init_conditions('h')=xdata(i,2);
+        
+        % evaluate steady state
+        ss=get_steady(sim,Delta,Max_iter); % evaluate steady state value
+
+        % get growth rate and ribosome mass fraction
+        par=sim.parameters;
+        m_a = ss(1);
+        m_r = ss(2);
+        p_a = ss(3);
+        R = ss(4);
+        tc = ss(5);
+        tu = ss(6);
+        Bcm = ss(7);
+        s = ss(8);
+        h = ss(9);
+        ss_het=ss(10 : (9+2*sim.num_het) ).';
+    
+        e=sim.form.e(par,tc); % translation elongation rate
+        kcmh=par('kcm').*h; % ribosome inactivation rate due to chloramphenicol
+    
+        % ribosome dissociation constants
+        k_a=sim.form.k(e,par('k+_a'),par('k-_a'),par('n_a'),kcmh);
+        k_r=sim.form.k(e,par('k+_r'),par('k-_r'),par('n_r'),kcmh);
+        k_het=ones(sim.num_het,1);
+
+        % ribosome dissociation constants for heterologous genes
+        if(sim.num_het>0)
+            for j=1:sim.num_het
+                k_het(i)=sim.form.k(e,...
+                sim.parameters(['k+_',sim.het.names{j}]),...
+                sim.parameters(['k-_',sim.het.names{j}]),...
+                sim.parameters(['n_',sim.het.names{j}]),...
+                kcmh);
+            end
+        end
+    
+        D=1+(m_a./k_a+m_r./k_r+sum(ss_het(1:sim.num_het)./k_het))./...
+            (1-par('phi_q')); % denominator in ribosome competition calculations
+        B=R.*(1-1./D); % actively translating ribosomes - INCLUDING HOUSEKEEPING GENES
+    
+        % growth rate
+        l=sim.form.l(par,e,B);
+        
+        % RECORD!
+        ymodel(i,1)=l; % record growth rate
+        ymodel(i,2)=(R+Bcm).*sim.parameters('n_r')./sim.parameters('M'); % record ribosome mass fraction
+    end
+end
